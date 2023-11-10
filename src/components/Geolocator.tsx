d2fb85207e1c75836123271b972a4ecd0331fe1e
Gabe Rogan <rogangabe@gmail.com>
2023-11-09 21:07:38 -0500

    feature: integrate react native permissions

--- src/components/Geolocator.tsx
+++ src/components/Geolocator.tsx
@@ -1,11 +1,44 @@
-import {Text, View} from 'react-native';
+import {useEffect, useState} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {requestLocationPermission} from '../services/geolocation';
 
 function Geolocator(): JSX.Element {
+  const [granted, setGranted] = useState(false);
+
+  useEffect(() => {
+    requestLocationPermission().then(result => setGranted(result));
+  }, []);
+
+  if (!granted) {
+    return (
+      <View style={styles.geoContainer}>
+        <Text>
+          Location permisssions not granted.{' '}
+          {Platform.OS === 'ios'
+            ? 'This feature is not yet available on iOS. See geolocation.ts for details.'
+            : ''}
+        </Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>TODO</Text>
+    <View style={styles.geoContainer}>
+      <Text>Location permisssions granted</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  geoContainer: {
+    margin: 8,
+    padding: 16,
+    backgroundColor: '#00007711',
+    gap: 8,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#00000044',
+  },
+});
 
 export default Geolocator;
---
+++ src/services/geolocation.ts
@@ -0,0 +1,60 @@
+// import {request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import {PermissionsAndroid, Platform} from 'react-native';
+
+export const requestLocationPermission = async () => {
+  try {
+    if (Platform.OS === 'ios') {
+      return false;
+    }
+
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'TakeHome Location Permission',
+        message:
+          'TakeHome App needs access to your location ' +
+          'to direct you to Star Wars World.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('You can use location');
+      return true;
+    } else {
+      console.log('Location permission denied');
+      return false;
+    }
+  } catch (err) {
+    console.warn(err);
+    return false;
+  }
+};
+
+// react-native-permissions is broken, adding the Permission-LocationWhenInUse.podspec causes a duplicate symbols for arm64 crash
+// export const requestLocationPermission = () => {
+//   request(PERMISSIONS.IOS.LOCATION_ALWAYS).then(result => {
+//     switch (result) {
+//       case RESULTS.UNAVAILABLE:
+//         console.log(
+//           'This feature is not available (on this device / in this context)',
+//         );
+//         break;
+//       case RESULTS.DENIED:
+//         console.log(
+//           'The permission has not been requested / is denied but requestable',
+//         );
+//         break;
+//       case RESULTS.LIMITED:
+//         console.log('The permission is limited: some actions are possible');
+//         break;
+//       case RESULTS.GRANTED:
+//         console.log('The permission is granted');
+//         break;
+//       case RESULTS.BLOCKED:
+//         console.log('The permission is denied and not requestable anymore');
+//         break;
+//     }
+//   });
+// };
